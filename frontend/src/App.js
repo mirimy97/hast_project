fbe62fd55846f9d06bdb9e8413cadf44e7eafa70
sungjeongeon <sje1942@naver.com>
2023-03-13 16:08:50 +0900

    geoJSON 반영

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,27 @@
 import { Canvas } from "@react-three/fiber";
 import Earth from "./pages/Earth";
+import World from "./pages/World";
 function App() {
   return (
+    // <Canvas
+    //   style={{
+    //     background:
+    //       "linear-gradient(180deg, #000000, #000000, #1b004f, #2a005e, #3c006c)",
+    //     width: "100vw",
+    //     height: "100vh",
+    //   }}
+    // >
+    //   <pointLight position={[10, 10, 10]} />
+    //   <mesh>
+    //     {/* 구 형상 생성 */}
+    //     {/* <sphereBufferGeometry /> */}
+    //     {/* 표면 생상, 재질 지정 */}
+    //     {/* <meshStandardMaterial color="yellow" /> */}
+    //   </mesh>
+    //   <Earth />
+    // </Canvas>
     <>
-      <Canvas
-        style={{
-          background:
-            "linear-gradient(180deg, #000000, #000000, #1b004f, #2a005e, #3c006c)",
-          width: "100vw",
-          height: "100vh",
-        }}
-      >
-        <pointLight position={[10, 10, 10]} />
-        <mesh>
-          {/* 구 형상 생성 */}
-          {/* <sphereBufferGeometry /> */}
-          {/* 표면 생상, 재질 지정 */}
-          {/* <meshStandardMaterial color="yellow" /> */}
-        </mesh>
-        <Earth />
-      </Canvas>
+      <World />
     </>
   );
 }
---
+++ frontend/src/pages/World.js
@@ -0,0 +1,73 @@
+import { useEffect, useMemo, useState } from "react";
+import * as d3 from "d3";
+import Globe from "react-globe.gl";
+import axios from "axios";
+
+function World() {
+  const [countries, setCountries] = useState({ features: [] });
+  const [hoverD, setHoverD] = useState();
+
+  const colorScale = d3.scaleSequentialSqrt(d3.interpolateYlOrRd);
+
+  const getGeoJson = () => {
+    return axios
+      .get("datasets/ne_110m_admin_0_countries.geojson")
+      .then((res) => res.data);
+  };
+
+  useEffect(() => {
+    const loadData = async () => {
+      // load data
+      const data = await getGeoJson();
+      setCountries(data);
+    };
+
+    // call async function
+    loadData();
+  }, []);
+
+  // GDP per capita (avoiding countries with small pop)
+  const getVal = (feat) =>
+    feat.properties.GDP_MD_EST / Math.max(1e5, feat.properties.POP_EST);
+
+  const maxVal = useMemo(
+    () => Math.max(...countries.features.map(getVal)),
+    [countries]
+  );
+
+  useEffect(() => {
+    console.log(countries);
+
+    colorScale.domain([0, maxVal]);
+  }, [countries]);
+
+  return (
+    <>
+      {countries.features && (
+        <Globe
+          globeImageUrl="./map/8k_earth_daymap.jpg"
+          backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
+          lineHoverPrecision={0}
+          polygonsData={countries.features.filter(
+            (d) => d.properties.ISO_A2 !== "AQ"
+          )}
+          polygonAltitude={(d) => (d === hoverD ? 0.12 : 0.06)}
+          polygonCapColor={(d) =>
+            d === hoverD ? "steelblue" : colorScale(getVal(d))
+          }
+          polygonSideColor={() => "rgba(0, 100, 0, 0.15)"}
+          polygonStrokeColor={() => "#111"}
+          polygonLabel={({ properties: d }) => `
+            <b>${d.ADMIN} (${d.ISO_A2}):</b> <br />
+            GDP: <i>${d.GDP_MD_EST}</i> M$<br/>
+            Population: <i>${d.POP_EST}</i>
+          `}
+          onPolygonHover={setHoverD}
+          polygonsTransitionDuration={300}
+        />
+      )}
+    </>
+  );
+}
+
+export default World;
