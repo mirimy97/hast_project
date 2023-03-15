96675bc4fa66e660ce7019fc8bdf473340690f0f
mirim <mrmy@kakao.com>
2023-03-15 17:37:40 +0900

    feat: 국가 클릭/뒤로가기 관련 기능

--- frontend/src/pages/World.js
+++ frontend/src/pages/World.js
@@ -1,94 +1,114 @@
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import * as d3 from "d3";
 import Globe from "react-globe.gl";
 import axios from "axios";
 
 function World() {
+  const globeRef = useRef();
   const [countries, setCountries] = useState({ features: [] });
   const [hoverD, setHoverD] = useState();
+  const [clickD, setClickD] = useState(null);
+  const [point, setPoint] = useState({
+    lat: 37.6,
+    lng: 124.2,
+    altitude: 2.5,
+  });
 
   const colorScale = d3.scaleSequentialSqrt(d3.interpolateYlOrRd);
 
-  const getGeoJson = () => {
+  const getGeoJson = useMemo(() => {
     return axios
       .get("geojson/ne_110m_admin_0_countries.geojson")
       .then((res) => res.data);
-  };
-
-  useEffect(() => {
-    const loadData = async () => {
-      // load data
-      const data = await getGeoJson();
-      setCountries(data);
-    };
-
-    // call async function
-    loadData();
   }, []);
 
-  // GDP per capita (avoiding countries with small pop)
-  const getVal = (feat) =>
-    feat.properties.GDP_MD_EST / Math.max(1e5, feat.properties.POP_EST);
-
-  const maxVal = useMemo(
-    () => Math.max(...countries.features.map(getVal)),
-    [countries]
-  );
-
   useEffect(() => {
-    console.log(countries);
+    // call async function
+    getGeoJson.then((data) => setCountries(data));
+  }, [getGeoJson]);
 
-    colorScale.domain([0, maxVal]);
-  }, [countries]);
+  // 클릭시 카메라 point 재설정
+  const clickRegion = (d) => {
+    console.log(d);
+    // clickD에 해당 구역 할당
+    setClickD(d);
 
-  // const clickRegion = useCallback(({ lat: endLat, lng: endLng }) => {
-  //   const { lat: startLat, lng: startLng } = prevCoords.current;
-  //   prevCoords.current = { lat: endLat, lng: endLng };
+    const bbox = d.bbox;
+    console.log(bbox);
+    // bbox = [경도시작(왼) 위도시작(위) 경도끝(오) 위도끝(밑)]
+    const lat = (bbox[1] + bbox[3]) / 2;
+    // const lng = (bbox[0] + bbox[2]) / 2;
+    const lng = bbox[2] - bbox[0] < 20 ? bbox[2] : (bbox[0] + 3 * bbox[2]) / 4;
 
-  //   // add and remove arc after 1 cycle
-  //   const arc = { startLat, startLng, endLat, endLng };
-  //   setArcsData(curArcsData => [...curArcsData, arc]);
-  //   setTimeout(() => setArcsData(curArcsData => curArcsData.filter(d => d !== arc)), FLIGHT_TIME * 2);
+    setPoint({
+      lat: lat,
+      lng: lng,
+      altitude:
+        bbox[2] - bbox[0] < 300
+          ? bbox[2] - bbox[0] < 40
+            ? bbox[2] - bbox[0] < 8
+              ? 0.25
+              : 0.5
+            : 1
+          : 2,
+    });
+  };
 
-  //   // add and remove start rings
-  //   const srcRing = { lat: startLat, lng: startLng };
-  //   setRingsData(curRingsData => [...curRingsData, srcRing]);
-  //   setTimeout(() => setRingsData(curRingsData => curRingsData.filter(r => r !== srcRing)), FLIGHT_TIME * ARC_REL_LEN);
+  // point state 변경 시 카메라 옮기기
+  useEffect(() => {
+    globeRef.current.pointOfView(point, 500);
 
-  //   // add and remove target rings
-  //   setTimeout(() => {
-  //     const targetRing = { lat: endLat, lng: endLng };
-  //     setRingsData(curRingsData => [...curRingsData, targetRing]);
-  //     setTimeout(() => setRingsData(curRingsData => curRingsData.filter(r => r !== targetRing)), FLIGHT_TIME * ARC_REL_LEN);
-  //   }, FLIGHT_TIME);
-  // }, []);
+    // 클릭해서 뷰 포인트 바뀐 경우 - 애니메이션 제한
+    if (clickD) {
+      setTimeout(function () {
+        globeRef.current.pauseAnimation();
+      }, 450);
+    }
+  }, [globeRef, point]);
+
+  // 뒤로가기 클릭 시 (나가기)
+  const backBtn = () => {
+    globeRef.current.resumeAnimation();
+    setClickD(null);
+    setPoint({
+      altitude: 2.5,
+    });
+  };
 
   return (
     <>
+      <button onClick={backBtn}>뒤로가기</button>
       {countries.features && (
         <Globe
+          ref={globeRef}
           globeImageUrl="map/8k_earth_daymap.jpg"
           backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
           lineHoverPrecision={0}
           polygonsData={countries.features.filter(
             (d) => d.properties.ISO_A2 !== "AQ"
           )}
-          polygonAltitude={(d) => (d === hoverD ? 0.03 : 0)}
-          // polygonCapColor={(d) =>
-          //   d === hoverD ? "steelblue" : colorScale(getVal(d))
-          // }
-
-          polygonCapColor={(d) => (d === hoverD ? "#7cc2b870" : "#ffffff00")}
-          polygonSideColor={() => "#ffffff00"}
+          polygonAltitude={(d) =>
+            clickD ? (d === clickD ? 0.008 : 0) : d === hoverD ? 0.03 : 0
+          }
+          polygonCapColor={(d) =>
+            // clickD 있으면
+            clickD
+              ? d === clickD
+                ? "#e6bb3c30"
+                : "#ffffff00"
+              : // clickD 없으면
+              d === hoverD
+              ? "#7cc2b870"
+              : "#ffffff00"
+          }
+          polygonSideColor={(d) => (d === clickD ? "#e6bb3c" : "#00000050")}
           polygonStrokeColor={() => "#00000080"}
           polygonLabel={({ properties: d }) => `
-            <b>${d.ADMIN} (${d.ISO_A2}):</b> <br />
-            GDP: <i>${d.GDP_MD_EST}</i> M$<br/>
-            Population: <i>${d.POP_EST}</i>
+            <b>${d.NAME} (${d.ISO_A2})</b>
           `}
-          onPolygonHover={setHoverD}
           polygonsTransitionDuration={300}
-          // onPolygonClick={clickRegion}
+          onPolygonHover={setHoverD}
+          onPolygonClick={clickRegion}
         />
       )}
     </>
