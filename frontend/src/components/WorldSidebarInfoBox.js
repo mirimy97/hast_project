5159ec9de200f5deb179ddbef9fa48e46e365234
mirim <mrmy@kakao.com>
2023-03-21 10:00:32 +0900

    feat: 국가 상세정보 스타일

--- frontend/src/components/WorldSidebarInfoBox.js
+++ frontend/src/components/WorldSidebarInfoBox.js
@@ -10,42 +10,48 @@ function WorldSidebarInfoBox({
   SUBREGION,
 }) {
   return (
-    <div className={styles.infobox}>
-      {/* 경제수준 */}
-      <div className={styles.flex}>
-        <img className={styles.icon} src="/assets/3d/man.png"></img>
-        <div style={{ width: "240px" }}>
-          <p className={styles.label}>추정 인구</p>
-          <p className={styles.value}>
-            {POP_EST}
-            <span className={styles.label}>만명</span>
-          </p>
-          <p>RANK {POP_RANK}</p>
+    <>
+      <div className={styles.infobox}>
+        {/* 경제수준 */}
+        <div className={styles.flex}>
+          <img className={styles.icon} src="/assets/3d/man.png"></img>
+          <div className={styles.columnCenter}>
+            <p className={styles.label}>추정 인구</p>
+            <p className={styles.value}>
+              {POP_EST}
+              <span className={styles.label}> 명</span>
+            </p>
+          </div>
         </div>
       </div>
       {/* 인구수 */}
-      <div className={styles.flex}>
-        <div style={{ width: "240px", textAlign: "end" }}>
-          <p className={styles.label}>GDP</p>
-          <p className={styles.value}>
-            {GDP}
-            <span className={styles.label}>$</span>
-          </p>
+      <div className={styles.infobox}>
+        <div style={{ justifyContent: "flex-end" }} className={styles.flex}>
+          <div style={{ textAlign: "end" }} className={styles.columnCenter}>
+            <p className={styles.label}>GDP</p>
+            <p className={styles.value}>
+              {GDP}
+              <span className={styles.label}> M$</span>
+            </p>
 
-          <p className={styles.label}>경제분류 : {ECONOMY}</p>
-          <p className={styles.label}>소득수준 : {INCOME_GRP}</p>
+            <p className={styles.label}>경제분류 : {ECONOMY}</p>
+            <p className={styles.label}>소득수준 : {INCOME_GRP}</p>
+          </div>
+          <img className={styles.icon} src="/assets/3d/coin.png"></img>
         </div>
-        <img className={styles.icon} src="/assets/3d/coin.png"></img>
       </div>
       {/* 대륙 */}
-      <div className={styles.flex}>
-        <img className={styles.icon} src="/assets/3d/location.png"></img>
-        <div style={{ width: "240px" }}>
-          <p className={styles.value}>{CONTINENT}</p>
-          <p className={styles.label}>{SUBREGION}</p>
+      <div className={styles.infobox}>
+        <div className={styles.flex}>
+          <img className={styles.icon} src="/assets/3d/location.png"></img>
+          <div className={styles.columnCenter}>
+            <p className={styles.value}>
+              {CONTINENT}, {SUBREGION}
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
