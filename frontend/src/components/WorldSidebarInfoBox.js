061234fdce1ba67e7ff3271fb7a26f3f0eba0ba6
mirim <mrmy@kakao.com>
2023-03-21 11:17:31 +0900

    feat: 국가 상세정보 국제화 설정

--- frontend/src/components/WorldSidebarInfoBox.js
+++ frontend/src/components/WorldSidebarInfoBox.js
@@ -1,4 +1,5 @@
 import styles from "./WorldSidebar.module.css";
+import { useTranslation, withTranslation } from "react-i18next";
 
 function WorldSidebarInfoBox({
   GDP,
@@ -9,6 +10,7 @@ function WorldSidebarInfoBox({
   CONTINENT,
   SUBREGION,
 }) {
+  const { t, i18n } = useTranslation();
   return (
     <>
       <div className={styles.infobox}>
@@ -16,10 +18,10 @@ function WorldSidebarInfoBox({
         <div className={styles.flex}>
           <img className={styles.icon} src="/assets/3d/man.png"></img>
           <div className={styles.columnCenter}>
-            <p className={styles.label}>추정 인구</p>
+            <p className={styles.label}>{t("nation-info.Population")}</p>
             <p className={styles.value}>
               {POP_EST}
-              <span className={styles.label}> 명</span>
+              <span className={styles.label}> {t("nation-info.people")}</span>
             </p>
           </div>
         </div>
@@ -27,15 +29,16 @@ function WorldSidebarInfoBox({
       {/* 인구수 */}
       <div className={styles.infobox}>
         <div style={{ justifyContent: "flex-end" }} className={styles.flex}>
-          <div style={{ textAlign: "end" }} className={styles.columnCenter}>
+          <div className={styles.columnCenter}>
             <p className={styles.label}>GDP</p>
             <p className={styles.value}>
               {GDP}
               <span className={styles.label}> M$</span>
             </p>
 
-            <p className={styles.label}>경제분류 : {ECONOMY}</p>
-            <p className={styles.label}>소득수준 : {INCOME_GRP}</p>
+            <p className={styles.label}>
+              {t(`economy.${ECONOMY[0]}`)} , {t(`income.${INCOME_GRP[0]}`)}
+            </p>
           </div>
           <img className={styles.icon} src="/assets/3d/coin.png"></img>
         </div>
@@ -46,7 +49,10 @@ function WorldSidebarInfoBox({
           <img className={styles.icon} src="/assets/3d/location.png"></img>
           <div className={styles.columnCenter}>
             <p className={styles.value}>
-              {CONTINENT}, {SUBREGION}
+              {t(`continent.${CONTINENT}`)}
+              <span style={{ fontSize: "1.1rem", marginLeft: "5px" }}>
+                ({t(`continent.${SUBREGION}`)})
+              </span>
             </p>
           </div>
         </div>
@@ -55,4 +61,4 @@ function WorldSidebarInfoBox({
   );
 }
 
-export default WorldSidebarInfoBox;
+export default withTranslation()(WorldSidebarInfoBox);
