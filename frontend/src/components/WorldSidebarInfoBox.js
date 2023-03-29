4f1e800b7c9fd72dd68c500dee3f04a7b00476f9
mirim <mrmy@kakao.com>
2023-03-29 17:29:16 +0900

    feat: globe 사이드바 레이아웃 수정

--- frontend/src/components/WorldSidebarInfoBox.js
+++ frontend/src/components/WorldSidebarInfoBox.js
@@ -15,53 +15,45 @@ function WorldSidebarInfoBox({
 
   return (
     <>
-      <div className={styles.infobox}>
+      <div className={styles.infoOuter}>
         {/* 경제수준 */}
-        <div className={styles.flex}>
+        <div className={styles.infoboxC} style={{ marginRight: "7px" }}>
           <img className={styles.icon} src="/assets/3d/man.png"></img>
-          <div className={styles.columnCenter}>
-            <p className={styles.label}>{t("nation-info.Population")}</p>
-            <p className={styles.value}>
-              {POP_EST.toLocaleString("en-US")}
-              <span className={styles.subtext}> {t("nation-info.people")}</span>
-            </p>
-          </div>
+
+          <p className={styles.label} style={{ marginTop: "20px" }}>
+            {t("nation-info.Population")}
+          </p>
+          <p className={styles.value}>
+            {POP_EST.toLocaleString("en-US")}
+            <span className={styles.subtext}> {t("nation-info.people")}</span>
+          </p>
         </div>
-      </div>
-      {/* 인구수 */}
-      <div className={styles.infobox}>
-        <div className={styles.flex}>
-          <div
-            style={{ alignItems: "center", marginLeft: "30px" }}
-            className={styles.columnCenter}
-          >
-            <div>
-              <p className={styles.label}>GDP</p>
-              <p className={styles.value}>
-                {GDP.toLocaleString("en-US")}
-                <span className={styles.subtext}> M$</span>
-              </p>
-              <p className={styles.subtext}>
-                {t(`economy.${ECONOMY[0]}`)} , {t(`income.${INCOME_GRP[0]}`)}
-              </p>
-            </div>
-          </div>
+        {/* 인구수 */}
+        <div className={styles.infoboxC} style={{ marginLeft: "7px" }}>
           <img className={styles.icon} src="/assets/3d/coin.png"></img>
+          <p className={styles.label} style={{ marginTop: "10px" }}>
+            GDP
+          </p>
+          <p className={styles.value}>
+            {GDP.toLocaleString("en-US")}
+            <span className={styles.subtext}> M$</span>
+          </p>
+          <p className={styles.subtext}>{t(`economy.${ECONOMY[0]}`)}</p>
+          <p className={styles.subtext}>{t(`income.${INCOME_GRP[0]}`)}</p>
         </div>
       </div>
       {/* 대륙 */}
       <div className={styles.infobox}>
-        <div className={styles.flex}>
-          <img className={styles.icon} src="/assets/3d/location.png"></img>
-          <div className={styles.columnCenter}>
-            <p className={styles.value}>{t(`continent.${CONTINENT}`)}</p>
-            <p
-              style={{ fontSize: "1rem", marginLeft: "5px" }}
-              className={styles.value}
-            >
-              ({t(`continent.${SUBREGION}`)})
-            </p>
-          </div>
+        <img className={styles.icon} src="/assets/3d/location.png"></img>
+
+        <div className={styles.columnCenter}>
+          <p className={styles.value}>{t(`continent.${CONTINENT}`)}</p>
+          <p
+            style={{ fontSize: "1rem", marginLeft: "5px" }}
+            className={styles.value}
+          >
+            ({t(`continent.${SUBREGION}`)})
+          </p>
         </div>
       </div>
     </>
