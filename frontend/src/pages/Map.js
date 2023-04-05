556f7ab49b0ed0d3303a4e2267db6da1a99acc1b
sshh121 <lc307715@gmail.com>
2023-04-05 09:41:01 +0900

    Fix: https 수정

--- frontend/src/pages/Map.js
+++ frontend/src/pages/Map.js
@@ -58,7 +58,7 @@ export default function Map() {
   const [dangerList, setDangerList] = useState([]);
   useEffect(() => {
     axios
-      .get("http://j8e106.p.ssafy.io:8080/api/info/dots")
+      .get("https://apitest.hastmap.duckdns.org/api/info/dots")
       .then((res) => {
         if (res.data.resultCode === "SUCCESS") {
           // console.log(res.data.result)
@@ -74,7 +74,7 @@ export default function Map() {
   useEffect(() => {
     if (countryInfo !== null) {
       axios
-        .get(`http://j8e106.p.ssafy.io:8080/api/articles/${countryInfo.FIPS}`)
+        .get(`https://apitest.hastmap.duckdns.org/api/articles/${countryInfo.FIPS}`)
         .then((res) => {
           if (res.data.resultCode === "SUCCESS") {
             console.log(res.data.result);
